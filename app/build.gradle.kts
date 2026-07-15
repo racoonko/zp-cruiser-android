3f03af977230a2008747d393141da7ab7fb1bc94
racoonko <74937695+racoonko@users.noreply.github.com>
2026-07-16 01:45:14 +0200

    Create app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "com.racoonko.zpcruiser"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.racoonko.zpcruiser"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+    // Google Maps
+    implementation("com.google.maps.android:maps-compose:6.0.0")
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
+}
